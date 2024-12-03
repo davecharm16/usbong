baf6ae7a422eeaa1bb186d95d5b18efe9ae0dde0
Dave Charm Bulaquena <davecharm16@gmail.com>
2024-12-03 13:49:39 +0800

    feat: image background on the app

--- src/core/utils/constants.ts
+++ src/core/utils/constants.ts
@@ -2,7 +2,7 @@ const colors = {
   gradientStart: '#BEFFD0', // Light green
   gradientEnd: '#FFFFFF', // White
   inputBackground: '#FFFFFF',
-  inputBorder: '#CCCCCC',
+  inputBorder: '#000',
   placeholderText: '#777777',
   buttonBackground: '#4CAF50',
   buttonText: '#FFFFFF',
--- src/features/auth/screens/Login.tsx
+++ src/features/auth/screens/Login.tsx
@@ -1,12 +1,89 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {colors} from '../../../core/utils/constants'; // Adjust the path based on your file structure
 
-function Login() {
+const LoginScreen = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  )
-}
+    <LinearGradient
+      colors={[colors.gradientStart, colors.gradientEnd]}
+      style={styles.gradient}>
+        <View style={styles.container}>
+          <Text style={styles.title}>USBONG</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            placeholderTextColor={colors.placeholderText}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry
+            placeholderTextColor={colors.placeholderText}
+          />
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+        </View>
+        <ImageBackground
+          source={require('../../../assets/login/bg.png')}
+          resizeMode="cover"
+          style={styles.image}
+          height={20}
+        />
+    </LinearGradient>
+  );
+};
 
-export default Login;
+const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+  },
+  image : {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 40,
+    color: colors.titleText,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderRadius: 8,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: colors.inputBorder,
+  },
+  button: {
+    height: 40,
+    backgroundColor: colors.buttonBackground,
+    paddingHorizontal: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: colors.buttonText,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default LoginScreen;
